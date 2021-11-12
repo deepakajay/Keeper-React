6ea462ef372a3d7ad31526a91d78eab919417fa8
deepak k <deepakajay77@gmail.com>
2021-11-12 15:37:34 +0530

    Updates

---
+++ precache-manifest.84ba7e43a812281ba336975919451d1c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006cfea5c72d44a77bc0917ef4576bf0",
+    "url": "/Keeper-React/index.html"
+  },
+  {
+    "revision": "5a375caa7684772f30bf",
+    "url": "/Keeper-React/static/js/2.906c33bb.chunk.js"
+  },
+  {
+    "revision": "14c095a212e75f84ee09",
+    "url": "/Keeper-React/static/js/main.1d6d5857.chunk.js"
+  },
+  {
+    "revision": "5d0b392a3bc520a73c05",
+    "url": "/Keeper-React/static/js/runtime-main.c8cb89be.js"
+  }
+]);
